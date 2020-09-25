8eb460c8174be10ebe88c142c99906c0d5d201fe
Richard <729381+NogginBox@users.noreply.github.com>
2020-09-25 13:36:52 +0100

    Added sample site and blank admin theme
    .

---
+++ Cqc.Orchard.GdsAdminTheme/Manifest.cs
@@ -0,0 +1,13 @@
+using OrchardCore.DisplayManagement.Manifest;
+using OrchardCore.Modules.Manifest;
+
+
+[assembly: Theme(
+    Name = "CQC Orchard GDS Admin Theme",
+    BaseTheme = "The Admin Theme",
+    Author = "CQC",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "The CQC GDS Admin theme includes all you need to make GDS pages",
+    Tags = new[] { ManifestConstants.AdminTag }
+)]
