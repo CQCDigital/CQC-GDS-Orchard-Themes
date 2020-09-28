fffad20636181543552ddedd5babe815e9ca99e6
Richard <729381+NogginBox@users.noreply.github.com>
2020-09-28 16:46:44 +0100

    Added TinyGds

--- Cqc.Orchard.GdsAdminTheme/Manifest.cs
+++ Cqc.Orchard.GdsAdminTheme/Manifest.cs
@@ -4,10 +4,10 @@ using OrchardCore.Modules.Manifest;
 
 [assembly: Theme(
     Name = "CQC Orchard GDS Admin Theme",
-    BaseTheme = "The Admin Theme",
+    BaseTheme = "TheAdmin",
     Author = "CQC",
-    Website = "https://orchardcore.net",
+    Website = "https://github.com/CQCDigital/GDS-CQC-Orchard-Admin-Theme",
     Version = "0.0.1",
-    Description = "The CQC GDS Admin theme includes all you need to make GDS pages",
+    Description = "This theme includes all you need to make GDS pages.",
     Tags = new[] { ManifestConstants.AdminTag }
 )]
---
+++ Cqc.Orchard.GdsAdminTheme/wwwroot/tinymce/plugins/gds_para/plugin.js
@@ -0,0 +1,66 @@
+﻿/**
+ * Copyright (c) Care Quality Commission, Inc. All rights reserved.
+ * Version: 1.0.0
+ */
+tinymce.PluginManager.add('gds_para', function (editor, url) {
+
+    var openDialog = function () {
+        return editor.windowManager.open({
+            title: 'GDS Paragraph',
+            body: {
+                type: 'panel',
+                items: [
+                    {
+                        type: 'textarea',
+                        name: 'title',
+                        label: 'Text'
+                    }
+                ]
+            },
+            buttons: [
+                {
+                    type: 'cancel',
+                    text: 'Close'
+                },
+                {
+                    type: 'submit',
+                    text: 'Save',
+                    primary: true
+                }
+            ],
+            onSubmit: function (api) {
+                var data = api.getData();
+                // Insert content when the window form is submitted
+                editor.insertContent("<p class='govuk-body'>" + data.title + "</p>");
+                api.close();
+            }
+        });
+    };
+
+    // Add a button that opens a window
+    editor.ui.registry.addButton('gds_para', {
+        text: '&lt;/p&gt;',
+        onAction: function () {
+            // Open window
+            openDialog();
+        }
+    });
+
+    // Adds a menu item, which can then be included in any menu via the menu/menubar configuration
+    editor.ui.registry.addMenuItem('gds_para', {
+        text: 'GDS Paragraph',
+        onAction: function () {
+            // Open window
+            openDialog();
+        }
+    });
+
+    return {
+        getMetadata: function () {
+            return {
+                name: 'GDS Paragraph',
+                url: 'http://exampleplugindocsurl.com'
+            };
+        }
+    };
+});
